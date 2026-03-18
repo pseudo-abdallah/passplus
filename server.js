1a3cf8c459a0b3a0eb606c9b3ddbd07e35a0c984
Abdallah abdelmonem ibrahim <s2211138153@ku.edu.kw>
2026-03-19 02:39:35 +0300

    add custom express server and start script for DO

---
+++ server.js
@@ -0,0 +1,20 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
+// The catchall handler for React Router
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// Use PORT from environment (DigitalOcean expects 8080)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(\Server running on port \\);
+});
